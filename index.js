79bada955858f23265fae2b367b71c09a9b2ba4c
xiaohuanghuang <1490451648@qq.com>
2018-09-29 11:53:29 +0800

    Fix

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import { AppRegistry, YellowBox } from 'react-native';
 import React, { Component } from 'react';
 import './shim';
-import App from './app/App';
+// import App from './app/App';
+import Lauch from './Launch'
 YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader', 'Module RNRandomBytes']);
 
-AppRegistry.registerComponent('rnWallet', () => App);
+// AppRegistry.registerComponent('rnWallet', () => App);
+AppRegistry.registerComponent('rnWallet', () => Lauch);
